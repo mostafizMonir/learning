c72db82be95a06345cba5db720646d22c23b5378
monir_mostafizur <mostafizur.rahman@streamstech.com>
2024-11-25 21:44:17 +0600

    niget added

---
+++ Auth/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [Route("redirect")]
+        [HttpGet]
+        public ActionResult Redirect()
+        {
+            return Ok("success");
+        }
+    }
+}
--- Auth/Program.cs
+++ Auth/Program.cs
@@ -1,20 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+  
 }
 
+
+
+
 app.UseHttpsRedirection();
+
+
 
 var summaries = new[]
 {
